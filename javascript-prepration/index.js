a6bb6be469e02377ab3cebcc6e62fb22f0c9d4fe
HunainAfzal-dev <hunain.pc933@gmail.com>
2025-09-11 16:14:11 +0500

    initialization

---
+++ javascript-prepration/index.js
@@ -0,0 +1,8 @@
+// Everything in Javascript happens inside an execcution context.
+
+// Memory =>  key: values , functions; also called= variable enviroment
+
+// code => executes single line of code; also called= thread of execution
+
+
+// javascript is a "synchronous" "single threaded" language 
